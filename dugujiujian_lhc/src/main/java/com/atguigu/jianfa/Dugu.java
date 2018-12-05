d75a1a1e613d6081afe63481bc63afc2797c2be3
ldsgood <1935279699@qq.com>
2018-12-05 20:22:08 +0800

    独孤九剑

---
+++ dugujiujian_lhc/src/main/java/com/atguigu/jianfa/Dugu.java
@@ -0,0 +1,14 @@
+package com.atguigu.jianfa;
+
+public class Dugu {
+    public static void main(String[] args) {
+        System.out.println("hello good yes");
+        System.out.println("1111111111 yes");
+        System.out.println("hello good yes");
+        System.out.println("hello good yes");
+        System.out.println("hello good yes");
+        System.out.println("hello good yes");
+        System.out.println("hello good yes");
+        System.out.println("hello good yes");
+    }
+}
